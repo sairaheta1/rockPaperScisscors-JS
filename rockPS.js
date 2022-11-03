f8e66f866c0e8ece6081a5cc54ec7ac04ef6eeeb
saira <sairaheta1@gmail.com>
2022-11-03 13:50:39 -0700

    added function to get computer's selection

--- rockPS.js
+++ rockPS.js
@@ -1,1 +1,4 @@
-console.log("hello world!");
+function getComputerSelection() {
+  return (Math.floor(math.random() * 10) % 3) + 1; //generate random number between 0 and 1, multiply by 10 to obtain integer, (mod 3 + 1) to get a number between 1 and 3
+
+}
