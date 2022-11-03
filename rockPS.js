5e9885e98d4ec3f321ac0bf3ec65229dc6349e18
saira <sairaheta1@gmail.com>
2022-11-03 14:03:57 -0700

    created function for users selection

--- rockPS.js
+++ rockPS.js
@@ -1,4 +1,14 @@
 function getComputerSelection() {
-  return (Math.floor(math.random() * 10) % 3) + 1; //generate random number between 0 and 1, multiply by 10 to obtain integer, (mod 3 + 1) to get a number between 1 and 3
-
+  return (Math.floor(Math.random() * 10) % 3) + 1; //generate random number between 0 and 1, multiply by 10 to obtain integer, (mod 3 + 1) to get a number between 1 and 3
 }
+
+function getUserSelection() {
+  let selection = prompt("Rock, Paper, or Scissors? Choose wisely.");
+
+  let result = selection.toLowerCase(); // this won't work if its null, can add that change later on
+  
+  //is there a way to have a default value if nothing or blank is typed in?
+return result
+}
+console.log(getComputerSelection());
+console.log(getUserSelection());
