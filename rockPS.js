8304791a7463b8ed5fc9bce117bfcb9c3358f613
Sam iraheta <sairaheta1@gmail.com>
2022-12-15 11:19:38 -0800

    added event listeners to buttons; deleted game function; modified playRound function

--- rockPS.js
+++ rockPS.js
@@ -1,35 +1,18 @@
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', ()) => {
-    
-  }
-})
+  button.addEventListener('click', () => {
+    playerSelection = button.id;
+    playRound(playerSelection, getComputerSelection());
+  });
+});
+
+
 
 
 function getComputerSelection() {
   return (Math.floor(Math.random() * 10) % 3) + 1; //generate random number between 0 and 1, multiply by 10 to obtain integer, (mod 3 + 1) to get a number between 1 and 3
 }
-
-function getUserSelection() {
-  const btn
-  let selection = prompt("Rock, Paper, or Scissors? Choose wisely."); //consider input validation
-//can repeat loop if user does not select a proper choice
-  let result = selection.toLowerCase(); // this won't work if its null, can add that change later on
-  
-  //is there a way to have a default value if nothing or blank is typed in?
-return result
-}
-function convertToNum(string) {
-  if (string === "rock") {
-    return 1;
-  }
-  else if (string === "paper") {
-    return 2;
-  }
-  return 3;
-}
-  
 //values will already be converted to their number representations once this function is called
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
@@ -58,12 +41,3 @@ function playRound(playerSelection, computerSelection) {
     console.log("You won! Scissors beats Paper!");
   }
 }
-
-function game() {
-
-  let computerSelection = getComputerSelection();
-  let playerSelection = convertToNum(getUserSelection()); 
-
-  playRound(playerSelection, computerSelection);
-}
-console.log(game());
